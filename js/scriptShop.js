7c6ac35092328d85a0394f434d48a250d055fc70
ngoctrunght19 <ngoctrunght19@gmail.com>
2016-10-22 12:50:08 +0700

    thêm slider vào trang chủ

--- js/scriptShop.js
+++ js/scriptShop.js
@@ -1,80 +1,36 @@
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(50);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-});
-jQuery(document).ready(function ($) {
-            
-            var jssor_options = {
-              $AutoPlay: true,
-              $AutoPlaySteps: 4,
-              $SlideDuration: 160,
-              $SlideWidth: 200,
-              $SlideSpacing: 3,
-              $Cols: 5,
-              $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$,
-                $Steps: 5
+$(document).ready(function() {
+      var owl = $(".custom-owl");
+
+      owl.owlCarousel({
+          autoplay: 10,
+          loop:true,
+          margin: 10,
+          nav: true,
+          navText: ['<span>&#139;</span>','&#155;'],
+          stopOnHover: true,
+          responsive:{
+              0:{
+                  items:2
               },
-              $BulletNavigatorOptions: {
-                $Class: $JssorBulletNavigator$,
-                $SpacingX: 1,
-                $SpacingY: 1
+              320:{
+                  items:3
+              },
+              600:{
+                  items:4
+              },
+              1000:{
+                  items:5
               }
-            };
-            
-            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_options);
-            var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_options);
-            
-            var jssor_3_slider = new $JssorSlider$("jssor_3", jssor_options);
-            //responsive code begin
-            //you can remove responsive code if you don't want the slider scales while window resizing
-            function ScaleSlider() {
-                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-                if (refSize) {
-                    refSize = Math.min(refSize, 1200);
-                    jssor_1_slider.$ScaleWidth(refSize);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
+          }
+      });
 
-            function ScaleSlider2() {
-                var refSize = jssor_2_slider.$Elmt.parentNode.clientWidth;
-                if (refSize) {
-                    refSize = Math.min(refSize, 1200);
-                    jssor_2_slider.$ScaleWidth(refSize);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
-              function ScaleSlider3() {
-                var refSize = jssor_3_slider.$Elmt.parentNode.clientWidth;
-                if (refSize) {
-                    refSize = Math.min(refSize, 1200);
-                    jssor_3_slider.$ScaleWidth(refSize);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
-
-            ScaleSlider();
-            $(window).bind("load", ScaleSlider);
-            $(window).bind("resize", ScaleSlider);
-            $(window).bind("orientationchange", ScaleSlider);
-            ScaleSlider2();
-            $(window).bind("load", ScaleSlider2);
-            $(window).bind("resize", ScaleSlider2);
-            $(window).bind("orientationchange", ScaleSlider2);
-            ScaleSlider3();
-            $(window).bind("load", ScaleSlider3);
-            $(window).bind("resize", ScaleSlider3);
-            $(window).bind("orientationchange", ScaleSlider3);
-            //responsive code end
-});
+      $(".slider .next").click(function(){
+        owl.trigger('owl.next');
+      })
+      $(".slider .prev").click(function(){
+        owl.trigger('owl.prev');
+      })
+    });
 // Get the modal
     var modal = document.getElementById('idmodal');
 
